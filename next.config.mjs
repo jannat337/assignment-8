88e92e90378778b1108871644185b98d222f2782
jannat337 <jannatulferdauz3@gmail.com>
2026-05-30 22:20:32 +0600

    Fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     serverExternalPackages: ['better-sqlite3'],
-    server: {
-        port: process.env.PORT || 3000,
-    }
 };
 
 export default nextConfig;
